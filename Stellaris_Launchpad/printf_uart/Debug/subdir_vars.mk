3167a828f79065c9a2867dcdf354f05291c038b1
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2014-10-26 12:17:44 +0530

    Initial release

---
+++ Stellaris_Launchpad/printf_uart/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lm4f120h5qr.cmd 
+
+C_SRCS += \
+../main.c \
+../uart_debug.c 
+
+OBJS += \
+./main.obj \
+./uart_debug.obj 
+
+C_DEPS += \
+./main.pp \
+./uart_debug.pp 
+
+C_DEPS__QUOTED += \
+"main.pp" \
+"uart_debug.pp" 
+
+OBJS__QUOTED += \
+"main.obj" \
+"uart_debug.obj" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../uart_debug.c" 
+
+
